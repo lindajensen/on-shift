34ba969504e4d6d2e895d1de03ba31385e1622b8
Linda Jensen <linda.jensen81@me.com>
2026-04-16 13:34:38 +0200

    Add route to LandingPage in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,21 @@ import {
 } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
 
 import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      {/* <Route index element={<HomePage />} /> */}
-    </Route>
-  )
+    <>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<LandingPage />} />
+      </Route>
+
+      {/* WorkerLayout */}
+      {/* EmployerLayout */}
+    </>,
+  ),
 );
 
 function App() {
