ee12ecf752996caf9b39c37c9d2b6d470ea48cdc
Linda Jensen <linda.jensen81@me.com>
2026-04-16 16:29:11 +0200

    Import JobListing component in LandingPage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import HowItWorks from "../components/HowItWorks";
+import JobListings from "../components/JobListings";
 
 function LandingPage() {
   return (
     <>
       <Hero />
       <HowItWorks />
+      <JobListings />
     </>
   );
 }
