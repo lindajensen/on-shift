0a41dcd52e27f529fa0dc59a2b8c97ff6e49141e
Linda Jensen <linda.jensen81@me.com>
2026-04-19 11:13:38 +0200

    Add LoginNudge component with styling

---
+++ frontend/src/components/LoginNudge.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+import "../styles/LoginNudge.css";
+
+function LoginNudge() {
+  return (
+    <div className="login-nudge">
+      <p className="login-nudge__text">Logga in för att ansöka</p>
+      <Link className="btn btn--primary" to="/login">
+        Logga in
+      </Link>
+    </div>
+  );
+}
+
+export default LoginNudge;
