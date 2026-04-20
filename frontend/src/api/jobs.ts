b5d037509c7ed1f1efb9b12d59dd325a354370d4
Linda Jensen <linda.jensen81@me.com>
2026-04-20 11:13:27 +0200

    Add getJobById function

--- frontend/src/api/jobs.ts
+++ frontend/src/api/jobs.ts
@@ -3,3 +3,11 @@ import { mockJobs } from "../data/mockJobs";
 export async function getAllJobs() {
   return mockJobs;
 }
+
+export async function getJobById(id: string) {
+  const job = mockJobs.find((job) => job.id === Number(id));
+
+  if (!job) throw new Error("Inget jobb hittades");
+
+  return job;
+}
