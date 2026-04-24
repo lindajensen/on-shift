5b5a803e20cbb4e783e2f3f76ef9537f35a68b0d
Linda Jensen <linda.jensen81@me.com>
2026-04-24 14:45:41 +0200

    Add getInitials utils function

--- frontend/src/utils/text.ts
+++ frontend/src/utils/text.ts
@@ -1,3 +1,12 @@
 export function capitalize(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
+
+export function getInitials(name: string): string {
+  return name
+    .split(" ")
+    .filter((word) => /^[a-zA-ZåäöÅÄÖ]/.test(word))
+    .slice(0, 2)
+    .map((word) => word[0])
+    .join("");
+}
