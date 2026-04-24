8f2a5d94507cea1c1b1c0112baa8436023e5cdb9
Linda Jensen <linda.jensen81@me.com>
2026-04-24 10:21:53 +0200

    Switch from Client to Pool

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,15 +1,21 @@
-import { Client } from "pg";
+// import { Client } from "pg";
+import { Pool } from "pg";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const client = new Client({
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-client
-  .connect()
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => console.error("Database connection error:", err));
+// client
+//   .connect()
+//   .then(() => console.log("Connected to the database"))
+//   .catch((err) => console.error("Database connection error:", err));
 
-export default client;
+export default pool;
