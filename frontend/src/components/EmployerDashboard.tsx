0503f20451c09db877491c0ac9e5ece203dddfc3
Linda Jensen <linda.jensen81@me.com>
2026-04-25 17:17:52 +0200

    Add JobListingsPreview to EmployerDashboard

--- frontend/src/components/EmployerDashboard.tsx
+++ frontend/src/components/EmployerDashboard.tsx
@@ -1,7 +1,22 @@
-import "../styles/EmployerDashboard.css";
+import JobListingsPreview from "./JobListingsPreview";
+
+import "../styles/Dashboard.css";
 
 function EmployerDashboard() {
-  return <div>EmployerDashboard</div>;
+  return (
+    <section className="dashboard">
+      <div className="section__inner">
+        <header className="dashboard__header">
+          <h1 className="dashboard__title">Hej Restaurang Norrsken!</h1>
+          <p className="dashboard__subtitle">Är du att hitta personal?</p>
+        </header>
+
+        {/* Stats */}
+
+        <JobListingsPreview />
+      </div>
+    </section>
+  );
 }
 
 export default EmployerDashboard;
